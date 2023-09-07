aabae93446972f036cfc06c797587f208d0c84bf
TheGenocides <luke.genesis.hyder@gmail.com>
2023-09-07 10:50:22 +0700

    fix: onclick (again)

--- script/index.js
+++ script/index.js
@@ -1,7 +1,9 @@
-const listItems = document.getElementsByClassName("navlist")
-listItems.forEach(element => {
-    if (element )
-    element.addEventListener("click", (event) => {
-        window.open("http://localhost:5500/", "_self")
-    })
-});
+const listItems = document.getElementById("navitems")
+for (let index = 0; index < listItems.children.length; index++) {
+    let child = listItems.children[index];
+    if (child.id != "userprofile"){
+        child.addEventListener('click', (_event) => {
+            window.open('./', "_self");
+        })
+    }
+}
