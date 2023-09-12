a7eaf1f1facf887fb45d6f94af16d63370996a9e
TheGenocides <luke.genesis.hyder@gmail.com>
2023-09-12 09:49:41 +0700

    chore: Removed useless comments

--- script/index.js
+++ script/index.js
@@ -1,15 +1,5 @@
 const welcome = document.getElementById("welcome");
 
-// const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
-//     const { top, left, bottom, right } = el.getBoundingClientRect();
-//     const { innerHeight, innerWidth } = window;
-//     return partiallyVisible
-//       ? ((top > 0 && top < innerHeight) ||
-//           (bottom > 0 && bottom < innerHeight)) &&
-//           ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
-//       : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
-//   };
-
 setTimeout(() => {
     welcome.style.visibility = "visible"
     welcome.style.opacity = 1
